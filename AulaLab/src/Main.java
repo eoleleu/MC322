fd35d647302fdac186d4af7026a2cf4934b41556
eoleleu <l213437@dac.unicamp.br>
2023-03-16 17:03:51 -0300

    finish

--- AulaLab/src/Main.java
+++ AulaLab/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Cliente leandro = new Cliente("Leandro", "018.123.696-02", "15 de junho de 2002", 20, "unicampp");
+        Cliente leandro = new Cliente("Leandro", "456.123.696-02", "15 de junho de 2002", 20, "unicampp");
         System.out.println(leandro.validarCPF());
 
     }
